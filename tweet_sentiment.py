e1ba94d9c355e98fafc40799ccde320c0e6c9f78
pizzatom <nude.mit@gmail.com>
2017-01-28 14:35:04 -0500

    added read_tweet function

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def hw():
     print 'Hello, world!'
@@ -6,12 +7,26 @@ def hw():
 def lines(fp):
     print str(len(fp.readlines()))
 
+def sent_dict(file_name):
+    afinnfile = file_name
+    scores = {} # initialize dictionary
+    for line in afinnfile:
+            term, score = line.split("\t")  # the file is tab-deliminated
+            scores[term] = int(score)  # convert score to integer
+    return scores
+
+def read_tweets(tweet_file):
+    for line in tweet_file:
+        jtweet = json.loads(line)
+        print jtweet
+    #print jtweet[u'text'].encode('utf-8')
+
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    sentdict = sent_dict(sent_file)
+    read_tweets(tweet_file)
+
 
 if __name__ == '__main__':
     main()
