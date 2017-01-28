4de0fad3d4ce7738f894b0fa2ddfccc62342079d
pizzatom <nude.mit@gmail.com>
2017-01-28 15:05:21 -0500

    added sent_calc to score tweet sentiment

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -19,14 +19,24 @@ def read_tweets(tweet_file):
     for line in tweet_file:
         jtweet = json.loads(line)
         if 'text' in jtweet:
-            print jtweet['text'].encode('utf-8')
+            return jtweet['text'].encode('utf-8')
     #print jtweet[u'text'].encode('utf-8')
+
+def sent_calc(tweet,sent_dict):
+    score = 0
+    for word in tweet.split(" "):
+        if word in sent_dict:
+            score += sent_dict[word]
+    print score
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
     sentdict = sent_dict(sent_file)
-    read_tweets(tweet_file)
+    tweet = 'this is just a test of good faith'
+    sent_calc(tweet,sentdict)
+
+#    read_tweets(tweet_file)
 
 
 if __name__ == '__main__':
